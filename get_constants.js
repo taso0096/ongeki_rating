cb858be8edde3d5c151d7a4fd526e51f4d4f2ec4
taso0096 <48194637+taso0096@users.noreply.github.com>
2019-10-07 23:09:44 +0900

    Update get_constants.js

--- get_constants.js
+++ get_constants.js
@@ -1,9 +1,15 @@
 let constants = {};
-for (let i = 0; i <= $('.ui_article_entry table').length; i++) {
-  let musicsData = $('.ui_article_entry table').eq(i);
-  for (let j = 1; j < musicsData.find('tr').length; j++) {
-    let musicData = musicsData.find('tr').eq(j).children('td');
-    constants[musicData.eq(2)[0].textContent.replace(/[^a-zA-Z0-9\u30a0-\u30ff\u3040-\u309f\u30e0-\u9fcf]/g, '') + musicData.eq(0)[0].textContent] = musicData.eq(6)[0].textContent;
+$.ajax({
+  type: 'GET',
+  url: 'https://ongeki.gamerch.com/オンゲキ%20楽曲一覧（Lv順）高難易度',
+  dataType:'html'
+}).done(res => {
+  for (let i = 0; i < $('.ui_article_entry table', res).length; i++) {
+    let musicsData = $('.ui_article_entry table', res).eq(i);
+    for (let j = 1; j < musicsData.find('tr').length; j++) {
+      let musicData = musicsData.find('tr')[j];
+      constants[musicData.children[3].textContent.replace(/[^a-zA-Z0-9\u30a0-\u30ff\u3040-\u309f\u30e0-\u9fcf]/g, '') + musicData.children[0].textContent] = musicData.children[6].textContent;
+    }
   }
-}
+});
 console.log(constants);
